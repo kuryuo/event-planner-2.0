85320cbcee91f96a0c1a337be8d9d008001d5b7c
kuryuo <128883817+kuryuo@users.noreply.github.com>
2026-04-28 10:00:00 +0500

    add global antd typography overrides

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App.tsx';
 import {Provider} from 'react-redux';
+import {applyThemeFromStorage} from '@/hooks/ui/useTheme.ts';
+import './index.css';
+import './antd-typography-overrides.scss';
+import App from './App.tsx';
 import {store} from './store/store';
 import {ToastProvider} from '@/components/toast/ToastProvider.tsx';
 import {AntdAppProvider} from '@/providers/AntdAppProvider.tsx';
+
+applyThemeFromStorage();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
